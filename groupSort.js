65ed95c2a85c65e0fe5c01d8433b6d227da9ff6d
yilin1168 <43439248+yilin1168@users.noreply.github.com>
2024-03-05 16:28:57 +0800

    Update groupSort.js

--- groupSort.js
+++ groupSort.js
@@ -1,3 +1,4 @@
+//group-sort(row-col)
 let data = [
     { name: "Item A", column: "tag1", value: 10 },
     { name: "Item B", column: "tag1", value: 20 },
@@ -44,3 +45,56 @@ Object.keys(groupedData).forEach(column => {
 let sortedData = [].concat(...Object.values(groupedData));
 
 console.log(sortedData);
+
+
+
+
+//group-sort all(row-col)
+let data = [
+    { name: "Item A", column: "tag1", value: 10 },
+    { name: "Item B", column: "tag1", value: 20 },
+    { name: "Item C", column: "tag1", value: null },
+    { name: "Item D", column: "tag1", value: 15 },
+    { name: "Item A", column: "tag2", value: 10 },
+    { name: "Item B", column: "tag2", value: 20 },
+    { name: "Item C", column: "tag3", value: null },
+    { name: "Item D", column: "tag3", value: 15 },
+    { name: "Item A", column: "tag3", value: null },
+    { name: "Item B", column: "tag3", value: 13 },
+];
+
+// 步骤 1: 计算每个name的总和并添加column='all'
+let sumsByName = data.reduce((acc, {name, value}) => {
+    acc[name] = (acc[name] || 0) + (value || 0);
+    return acc;
+}, {});
+
+let allSumsData = Object.entries(sumsByName).map(([name, sum]) => ({
+    name, column: 'all', value: sum
+}));
+
+data = data.concat(allSumsData);
+
+// 步骤 2: 分组数据并计算sortedOrder
+let groupedData = data.reduce((acc, item) => {
+    acc[item.column] = acc[item.column] || [];
+    acc[item.column].push(item);
+    return acc;
+}, {});
+
+Object.keys(groupedData).forEach(column => {
+    let items = groupedData[column];
+    // 排除null，计算排序序号
+    items
+        .filter(item => item.value !== null)
+        .sort((a, b) => a.value - b.value)
+        .forEach((item, index) => {
+            item.sortedOrder = index + 1;
+        });
+});
+
+// 步骤 3: 将处理过的数据重新组合，可能需要根据需要进行额外的处理，比如移除重复项
+let processedData = [].concat(...Object.values(groupedData));
+
+console.log(processedData);
+
