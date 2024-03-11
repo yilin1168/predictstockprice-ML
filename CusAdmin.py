c691aa678a928b6730dea60137b4631fb2f6d7d5
yilin1168 <43439248+yilin1168@users.noreply.github.com>
2024-03-11 14:59:00 +0800

    Update CusAdmin.py

--- CusAdmin.py
+++ CusAdmin.py
@@ -1,16 +1,32 @@
 from django.contrib import admin
 
-class CustomModelAdmin(admin.ModelAdmin):
+class MultiDBModelAdminMixin:
+    # 指定用于操作的数据库别名
+    using = 'default'
+
     def save_model(self, request, obj, form, change):
+        # 指定对象保存到的数据库
         obj.save(using=self.using)
 
     def delete_model(self, request, obj):
+        # 指定从哪个数据库删除对象
         obj.delete(using=self.using)
 
     def get_queryset(self, request):
+        # 修改查询集以使用指定的数据库
         return super().get_queryset(request).using(self.using)
 
-    # 指定其他方法以适应非默认数据库...
-admin.site.register(MyModel1, CustomModelAdmin(using='db1'))
-admin.site.register(MyModel2, CustomModelAdmin(using='db2'))
+
+
+
+class CustomAdminForDB1(MultiDBModelAdminMixin, admin.ModelAdmin):
+    using = 'db1'
+
+class CustomAdminForDB2(MultiDBModelAdminMixin, admin.ModelAdmin):
+    using = 'db2'
+
+# 然后注册你的模型和自定义的 ModelAdmin
+admin.site.register(MyModel1, CustomAdminForDB1)
+admin.site.register(MyModel2, CustomAdminForDB2)
+
 
