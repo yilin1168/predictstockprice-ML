d4cdba47f908ee64808e4af2612a00ce57488d24
yilin1168 <43439248+yilin1168@users.noreply.github.com>
2024-04-09 16:08:24 +0800

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO assets_yourmodel(LP, Date, Deal, data1, data2, data3, data4)
+VALUES 
+    ('LP1', DATE('2023-12-26'), 'deal1', 15, 20, 25, 40),
+    ('LP1', DATE('2023-12-26'), 'deal2', 15, -20, 25, -40),
+    ('LP2', DATE('2023-12-26'), 'deal3', 15, 20, 25, -40),
+    ('LP2', DATE('2023-12-26'), 'deal4', 15, -20, -25, 40);
